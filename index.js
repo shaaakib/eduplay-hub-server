5b7e175e80fd9ff436d5fe2f195ed86b0dc00cdf
Muhammad Shakib Hossain <shakibhossainmuhammad@gmail.com>
2023-05-18 20:10:47 +0600

    delete function added this server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -40,6 +40,13 @@ async function run() {
     app.post('/addtoys', async (req, res) => {
       const addToys = req.body;
       const result = await toysCollection.insertOne(addToys);
+      res.send(result);
+    });
+
+    app.delete('/addtoys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toysCollection.deleteOne(query);
       res.send(result);
     });
 
